1ef49742493ce70d3a66907afad01f0b93338cc3
Sarah Speck <smspeck13@gmail.com>
2018-03-13 01:04:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function happyHolidays() {
-  return "Happy holidays!";
+  return ("Happy holidays!");
 }
 
 function happyHolidaysTo(name) {
-  return `Happy holidays, ${name}!`;
+  return (`Happy holidays, ${name}!`);
 }
 
 function happyHolidayTo(holiday, name) {
